78113aca1add95bdb4726e4d1abee46ce27db87f
muh-osman <mr.muhammed.osman@gmail.com>
2025-02-01 16:15:44 +0300

    Fix

--- wp-content/themes/cashif_theme/assets/js/ar-page357c.js
+++ wp-content/themes/cashif_theme/assets/js/ar-page357c.js
@@ -34,3 +34,52 @@ window.addEventListener("scroll", function () {
     // callBtn.classList.remove("hide-btn");
   }
 });
+
+// Submit form to google sheets
+const form = document.forms["submit-to-google-sheet"];
+const overlay = document.getElementById("form_overlay");
+const btn = document.getElementById("btn");
+
+// const scriptURL =
+//   "https://script.google.com/macros/s/AKfycbwJC8IQU595yg-BAOGCHSJ0Ql9g30w81645F0eOgOKmrDD9hC6kEFG3RCy7RZ2qzcNwwA/exec";
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  btn.innerHTML =
+    '<span class="spinner-border spinner-border-sm" aria-hidden="true"></span>';
+
+  //
+  let name = document.getElementById("name").value;
+  //
+  let email = document.getElementById("email").value;
+  //
+  let subject = document.getElementById("subject").innerHTML;
+  //
+  let note = document.getElementById("note").value;
+
+  try {
+    const res = await fetch("https", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        subject: subject,
+        note: note,
+      }),
+    });
+
+    if (res.ok) {
+      overlay.style.height = "100%";
+      btn.innerHTML = "تم الارسال";
+      btn.disabled = true;
+      btn.style.background = "#777";
+    }
+  } catch (error) {
+    console.error("Error!", error.message);
+    btn.innerHTML = "ارسال";
+    alert(error);
+  }
+});
